90f98ff0a2d97d241080f459145473bfcb58de88
chenluuli@gmail.com <chenluuli@gmail.com>
2016-09-19 20:06:39 +0800

    Site updated: 2016-09-19 20:06:32

---
+++ resume/js/main.js
@@ -0,0 +1,13 @@
+var runPage;
+runPage = new FullPage({
+    id: 'pageContain',
+    slideTime: 800,
+    effect: {
+        transform: {
+            translate: 'Y'    //垂直滚动，改为X则是水平滚动
+        },
+        opacity: [0, 1]
+    },
+    mode: 'wheel, touch, nav:navBar',
+    easing: 'ease'
+});
